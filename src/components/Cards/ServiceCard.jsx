3798c676dacbb4443f3930a70879ee3d736a4aaf
Samson <94833173+Superior212@users.noreply.github.com>
2023-06-27 20:37:09 +0100

    update on the homepage

--- src/components/Cards/ServiceCard.jsx
+++ src/components/Cards/ServiceCard.jsx
@@ -4,23 +4,6 @@ const ServiceCard = ({ img, title, text, bgColor }) => {
   const cardClasses = `${bgColor}`;
 
   return (
-<<<<<<< HEAD
-    <div className="flex items-center justify-center">
-      <div className="max-w-sm">
-        <div className="w-full h-56">
-          <img
-            className="object-cover w-full h-full rounded"
-            src={img}
-            alt="Card Image"
-          />
-        </div>
-        <div
-          className={`px-6 py-4 bg-[#DEF2F5] font-lato h-44 overflow-hidden ${cardClasses}`}
-        >
-          <div className="font-bold text-xl mb-2">{title}</div>
-          <p className="text-gray-700 text-base">{text}</p>
-        </div>
-=======
     <div className="max-w-sm">
       <div className="w-full">
         <img
@@ -34,7 +17,6 @@ const ServiceCard = ({ img, title, text, bgColor }) => {
       >
         <div className="font-bold text-xl mb-2">{title}</div>
         <p className="text-gray-700 text-base">{text}</p>
->>>>>>> 1db11abd503228034482907fdc889b1fcff37ea1
       </div>
     </div>
   );
